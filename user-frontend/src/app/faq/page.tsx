ff590b93d9702f29f80cd503c4df73a4ac84a4d7
HiHiTutor <hihitutor@gmail.com>
2025-05-12 17:18:44 +0800

    feat: 新增 recommendations、faq 靜態頁面佔位於 app router

---
+++ user-frontend/src/app/faq/page.tsx
@@ -0,0 +1,3 @@
+export default function FAQPage() {
+  return <div style={{padding: '2rem'}}>常見問題頁面建置中</div>;
+} 
---
+++ user-frontend/src/app/recommendations/page.tsx
@@ -0,0 +1,3 @@
+export default function RecommendationsPage() {
+  return <div style={{padding: '2rem'}}>推薦頁面建置中</div>;
+} 
