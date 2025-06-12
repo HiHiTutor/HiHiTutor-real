301d531292911151b254bdb3679ac08cc5975bee
HiHiTutor <hihitutor@gmail.com>
2025-06-12 16:29:12 +0800

    fix: 修正導師詳情路由配置

--- backend/routes/tutorRoutes.js
+++ backend/routes/tutorRoutes.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const tutorController = require('../controllers/tutorController');
+const { getTutors, getTutorDetail, getTutorProfile, updateTutorProfile } = require('../controllers/tutorController');
 const { verifyToken } = require('../middleware/auth');
 
 // 公開 API
-router.get('/', tutorController.getTutors);
-router.get('/detail/:tutorId', tutorController.getTutorDetail);
+router.get('/', getTutors);
+router.get('/detail/:tutorId', getTutorDetail);
 
 // 需要認證的 API
 router.use(verifyToken);
-router.get('/profile', tutorController.getTutorProfile);
-router.put('/profile', tutorController.updateTutorProfile);
+router.get('/profile', getTutorProfile);
+router.put('/profile', updateTutorProfile);
 
 module.exports = router; 
