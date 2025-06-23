37f79ec79c582c12e2a177cd891506cf9a93b6b5
HiHiTutor <hihitutor@gmail.com>
2025-06-23 13:10:14 +0800

    feat: 更新 next.config.js 支援 S3 圖片域名載入

--- backend/next.config.js
+++ backend/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['placehold.co'],
+    domains: [
+      'placehold.co',
+      'hihitutor-uploads.s3.ap-southeast-2.amazonaws.com'
+    ],
   },
 };
 
