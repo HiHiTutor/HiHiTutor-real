d4f341f266957f2a90d04852e7c92aca567f324a
HiHiTutor <hihitutor@gmail.com>
2025-06-28 16:33:07 +0800

    Remove ACL from S3 upload to fix AccessControlListNotSupported error

--- backend/uploadMiddleware.js
+++ backend/uploadMiddleware.js
@@ -75,8 +75,7 @@ const uploadToS3 = async (req, res) => {
       Bucket: BUCKET_NAME,
       Key: key,
       Body: req.file.buffer,
-      ContentType: req.file.mimetype,
-      ACL: 'public-read'
+      ContentType: req.file.mimetype
     });
 
     await s3Client.send(command);
